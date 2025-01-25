686023618e343b6576ae28225df248e1b599bf49
Abhishek Kuldeep <abhishekkuldeep236@gmail.com>
2025-01-25 20:10:48 +0530

    Added some content on homepage4 and its CSS

--- src/Homepage4.jsx
+++ src/Homepage4.jsx
@@ -1,10 +1,24 @@
 import DropdownMenu from "./Components/DropdownMenu"
+import style from './css_file/homepage4.module.css'
 
 function Homepage4() {
   return (
     <>
-        <div>Homepage4</div>
-        <DropdownMenu/>
+        <div className={style.body}>
+          <div className={style.main}>
+            <div className={style.image}>
+              <p>Our Process</p>
+            </div>
+
+            <div className={style["drop-down"]}>
+              <DropdownMenu/>
+              <DropdownMenu/>
+              <DropdownMenu/>
+            </div>
+
+          </div>
+
+        </div>
     </>
   )
 }
