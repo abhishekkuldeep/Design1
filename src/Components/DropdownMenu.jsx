4e2b59e93e0814efddb611123c68458491ed9724
Abhishek Kuldeep <abhishekkuldeep236@gmail.com>
2025-01-27 20:16:17 +0530

    Some changes in the dropdown card file and process page

--- src/Components/DropdownMenu.jsx
+++ src/Components/DropdownMenu.jsx
@@ -1,7 +1,25 @@
+import style from '../Css_file/dropdown.module.css'
 
 function DropdownMenu() {
   return (
-    <div>DropdownMenu</div>
+    <>
+      <div className={style.main}>
+        <div className={style.serialNo}>
+          <p>01</p>
+
+        </div>
+        <div className={style.title}>
+          <p>Responsible Tech Use</p>
+        </div>
+
+        <div className={style.content}>
+          
+
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
