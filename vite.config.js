093f8b111bfa8baa83dad74e1a6efd2b04251da4
Abhishek Kuldeep <abhishekkuldeep236@gmail.com>
2025-01-28 20:16:54 +0530

    Updated vite.config file and CSS in the hero.module.css file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  css:{
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss";` // If using SCSS variables
+      },
+    },
+    modules: {
+      scopeBehaviour: 'local',
+    },
+  },
 })
