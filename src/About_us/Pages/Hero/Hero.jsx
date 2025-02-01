ccdca6650b5463c722f21128833821ffde1429f3
Abhishek Kuldeep <abhishekkuldeep236@gmail.com>
2025-02-01 20:47:27 +0530

    Added the hero section of About Us

--- src/About_us/Pages/Hero/Hero.jsx
+++ src/About_us/Pages/Hero/Hero.jsx
@@ -1,17 +1,26 @@
-import Header from '../../../Header/Header'
 import style from './Hero.module.css'
+import img1 from '../../../Assets/Images/semicircle-logo.png'
+import img2 from '../../../Assets/Images/meet.png'
 
 function Hero() {
   return (
     <>
     <div className={style.main}>
-        <Header/>
         <div className={style.body}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui. Lo</p>
+            <div className={style.left}>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <span> Omnis sit, quisquam </span> culpa hic molestias sequi?</p>
+
+            </div>
+            <div className={style.right}>
+              <img src={img1} alt="" />
+
+            </div>
 
         </div>
+        
 
     </div>
+    <img src={img2} alt="" />
     </>
   )
 }
